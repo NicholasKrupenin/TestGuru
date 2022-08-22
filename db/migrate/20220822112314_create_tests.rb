c8c7adb5a88ba21638c6b950627f1fbea54974fa
NicholasKrupenin <krupenin.nikolay@gmail.com>
2022-08-22 15:31:39 +0400

    lesson_2_migrations

---
+++ db/migrate/20220822112314_create_tests.rb
@@ -0,0 +1,10 @@
+class CreateTests < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tests do |t|
+      t.string :title, null: false
+      t.integer :level, null: false
+      t.references :category, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
