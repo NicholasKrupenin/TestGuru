1757f9f74d178680d3eb39a3826738262833f0b1
NicholasKrupenin <krupenin.nikolay@gmail.com>
2022-08-23 19:51:35 +0400

    lesson_2_add_author

--- db/migrate/20220822112314_create_tests.rb
+++ db/migrate/20220822112314_create_tests.rb
@@ -4,6 +4,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
       t.string :title, null: false
       t.integer :level, null: false
       t.references :category, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_112456) do
+ActiveRecord::Schema.define(version: 2022_08_23_154654) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false, null: false
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2022_08_22_112456) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -39,8 +45,10 @@ ActiveRecord::Schema.define(version: 2022_08_22_112456) do
     t.string "title", null: false
     t.integer "level", null: false
     t.integer "category_id", null: false
+    t.integer "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -52,5 +60,6 @@ ActiveRecord::Schema.define(version: 2022_08_22_112456) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "authors"
   add_foreign_key "tests", "categories"
 end
