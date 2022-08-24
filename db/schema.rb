a0beb782d1eba3f1e58ca5ed53696aeaa527a1b8
NicholasKrupenin <krupenin.nikolay@gmail.com>
2022-08-24 07:10:23 +0400

    lesson_2_add_user_delauthor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_154654) do
+ActiveRecord::Schema.define(version: 2022_08_22_112456) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false, null: false
@@ -19,12 +19,6 @@ ActiveRecord::Schema.define(version: 2022_08_23_154654) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
-  end
-
-  create_table "authors", force: :cascade do |t|
-    t.string "body", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -60,6 +54,6 @@ ActiveRecord::Schema.define(version: 2022_08_23_154654) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
-  add_foreign_key "tests", "authors"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
