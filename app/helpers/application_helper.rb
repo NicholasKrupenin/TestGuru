650bed288413601933eded5db79d225f71fd5ee8
Nicholas Krupenin <krupenin.nikolay@gmail.com>
2023-03-30 15:37:59 +0400

    rails_views 1.3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,28 +1,17 @@
 module ApplicationHelper
-  def question_header
-    if @question.persisted?
-      "<h1>Edit #{@question.test.title} Question</h1>".html_safe
+  def question_header(question)
+    if question.persisted?
+      "Edit #{question.test.title} Question"
     else
-      "<h1>Create New #{@test.title} Question</h1>".html_safe
+      "Create New #{question.test.title} Question"
     end
   end
 
-  def github_url(author, repo)
-    link_to repo.to_s, "https://github.com/#{author}/#{repo}"
+  def github_url(author, repo, html_options = {rel: 'nofollow', rel: 'noopener'})
+    link_to repo.to_s, "https://github.com/#{author}/#{repo}", html_options
   end
 
   def now_year
     Time.current.year
-  end
-
-  def signature(author, repo, alt_name = nil)
-    alt_name ||= author
-    out = <<~HTML
-      <p>Year: #{now_year}</p>
-      <p>GitHub page: #{github_url(author, repo)}</p>
-      <p>Author: #{alt_name}</p>
-      <p>School: <a rel=nofollow rel=noopener href=\"http://thinknetica.com/\">Учебный проект в онлайн-школе Thinknetica</a></p>
-    HTML
-    out.html_safe
   end
 end
