255426a158c05522e497662f69da55a58d451134
Nicholas Krupenin <krupenin.nikolay@gmail.com>
2023-04-19 05:31:40 +0400

    authentication_user_sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :test_by_author, class_name: 'Test', foreign_key: :author_id,dependent: :destroy
+  has_many :test_by_author, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
