fd94e88997a63ff19c52be5b409f204146ace3d9
Nicholas Krupenin <krupenin.nikolay@gmail.com>
2023-06-03 02:50:59 +0400

    stage_1

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,9 +1,8 @@
 class Admin::TestsController < Admin::BaseController
-  before_action :find_test, only: %i[show edit update destroy]
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show edit update destroy update_inline]
 
-  def index
-    @tests = Test.all
-  end
+  def index; end
 
   def new
     @test = Test.new
@@ -30,12 +29,24 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   def destroy
     @test.destroy
     redirect_to admin_tests_path
   end
 
   private
+
+  def set_tests
+    @tests = Test.all
+  end
 
   def find_test
     @test = Test.find(params[:id])
