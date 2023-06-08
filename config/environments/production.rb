4faa289a73037239fbdf99c6bface9bca72868c0
Nicholas Krupenin <krupenin.nikolay@gmail.com>
2023-06-08 23:26:49 +0400

    add_ya_mail 1.16

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,20 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'https://guru-mail-tests.onrender.com' }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.yandex.ru',
+    port: 465,
+    user_name: ENV.fetch('YANDEX_USERNAME'),
+    password: ENV.fetch('YANDEX_PASSWORD'),
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    ssl: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
