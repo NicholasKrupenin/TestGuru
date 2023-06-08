b2ea2e909742a4c7347611a79325c14dbcfc8d27
Nicholas Krupenin <krupenin.nikolay@gmail.com>
2023-06-08 23:09:10 +0400

    add_ya_mail 1.15

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://guru-mail-tests.onrender.com'}
+  config.action_mailer.default_url_options = { host: 'smtp.yandex.ru', from: 'https://guru-mail-tests.onrender.com'}
   config.action_mailer.smtp_settings = {
     address: 'smtp.yandex.ru',
     port: 465,
